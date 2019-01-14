fec6e9fe27595db42eb4c475065cb3c3d0817f56
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-14 15:00:50 +0000

    taking print_statement out of bank account

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,19 +1,29 @@
+# frozen_string_literal: true
+
 class BankAccount
+
+  attr_reader :account_transactions
 
   def initialize(client_name)
     @client_name = client_name
     @balance = 0
+    @account_transactions = []
   end
 
   def deposit_money(amount)
     @balance += amount
-    @deposit_time = (Time.now + 1800).strftime("%d/%m/%Y")
+    @deposit_time = (Time.now + 1800).strftime('%d/%m/%Y')
+    deposit_transaction = [@deposit_time, 'credit', amount, @balance]
+    @account_transactions << deposit_transaction
     "You deposited £#{amount} on #{@deposit_time}. Your account balance is £#{@balance}"
   end
 
   def withdraw_money(amount)
     @balance -= amount
-    @withdrawal_time = (Time.now + 1800).strftime("%d/%m/%Y")
+    @withdrawal_time = (Time.now + 1800).strftime('%d/%m/%Y')
+    withdrawal_transaction = [@withdrawal_time, 'debit', amount, @balance]
+    @account_transactions << withdrawal_transaction
     "You withdraw £#{amount} on #{@withdrawal_time}. Your account balance is £#{@balance}"
   end
+
 end
