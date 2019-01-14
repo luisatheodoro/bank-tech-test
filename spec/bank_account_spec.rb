91545089807cfeee8368d91109b9d4eb8ddaf305
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-14 15:50:55 +0000

    mocking date for other tests

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -4,18 +4,19 @@ require 'bank_account'
 
 describe BankAccount do
   let(:luisa_account) { described_class.new('Luisa') }
+  let(:date) { double :date, formatted_current_date: '10/01/2019'}
 
   describe '#deposit_money' do
     it 'should send a confirmation of deposit amount, date and account balance' do
-    expect(luisa_account.deposit_money(20)).to eq 'You deposited £20 on 14/01/2019.'\
+    expect(luisa_account.deposit_money(20, date)).to eq 'You deposited £20 on 10/01/2019.'\
                                                   ' Your account balance is £20'
     end
   end
 
   describe '#withdraw money' do
     it 'should send a confirmation of withdrawal amount, date and account balance' do
-      luisa_account.deposit_money(20)
-      expect(luisa_account.withdraw_money(20)).to eq 'You withdraw £20 on 14/01/2019.'\
+      luisa_account.deposit_money(20, date)
+      expect(luisa_account.withdraw_money(20, date)).to eq 'You withdraw £20 on 10/01/2019.'\
                                                      ' Your account balance is £0'
     end
   end
