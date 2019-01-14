422f1e32cdffad65bb4e43ed049981c772374fe6
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-14 13:10:54 +0000

    rubocop formating

--- spec/feature_tests/user_can_withdraw_money_spec.rb
+++ spec/feature_tests/user_can_withdraw_money_spec.rb
@@ -1,9 +1,9 @@
 feature 'Withdraw money' do
   scenario 'Withdrawing money from the bank account'
-    it 'will withdraw the money from the bank account' do
-      my_account = BankAccount.new('Luisa')
-      my_account.deposit_money(20)
-      expect { my_account.withdraw_money(20) }.not_to raise_error
-    end
+  it 'will withdraw the money from the bank account' do
+    my_account = BankAccount.new('Luisa')
+    my_account.deposit_money(20)
+    expect { my_account.withdraw_money(20) }.not_to raise_error
+  end
 
 end
