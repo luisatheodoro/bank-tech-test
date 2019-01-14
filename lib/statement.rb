6c02f8047e6238359f3d9fbe1e5e062c43fd8804
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-14 18:04:52 +0000

    fixing print format

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,6 +1,6 @@
 class Statement
   def print_statement(bank_account)
-    puts 'Date || Credit || Debit || Amount || Balance'
+    puts 'Date || Credit || Debit || Balance'
     bank_account.account_transactions.reverse_each do |transaction|
       puts "#{transaction[0]} || #{transaction[1]} || #{transaction[2]} || #{transaction[3]}"
     end
