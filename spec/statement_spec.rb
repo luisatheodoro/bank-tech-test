0a7a366da9c565323b2c714fbe14076a4615e10f
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-14 17:40:03 +0000

    fixing test

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -11,7 +11,7 @@ describe Statement do
   describe '#print_statement' do
     it 'print statement showing transactions date, type, amount and balance' do
       expect(luisa_statement.print_statement(luisa_account)).to eq \
-        [['10/01/2019', 'credit', 200, 200], ['14/01/2019', 'debit', 100, 100]]
+        [['14/01/2019', 'credit', 200, 200], ['10/01/2019', 'debit', 100, 100]]
     end
   end
 end
