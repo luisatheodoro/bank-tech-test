34a2e72dd9aa11e409e1abe5d435bf126b4e116f
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-14 11:56:24 +0000

    improving tests

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,10 +1,11 @@
+require 'bank_account'
+
 describe BankAccount do
   let(:luisa_account) { described_class.new('Luisa') }
 
   describe "#deposit_money" do
-    it "Client is able to see the money in the account"
-    expect(luisa_account.deposit_money(20.00)).to eq "£20.00 was deposited"
+    it "Client is able to see the money in the account" do
+    expect(luisa_account.deposit_money(20)).to eq "You deposited £20 your account balance is £20"
+    end
   end
-
-
 end
--- spec/feature_tests/user_can_deposit_spec.rb
+++ spec/feature_tests/user_can_deposit_spec.rb
@@ -2,7 +2,7 @@ feature 'user can deposit' do
 
   it 'so that I can have my money safely stored I would like to deposit it in the bank' do
     my_account = BankAccount.new('Luisa')
-    expect { my_account.deposit_money }.not_to raise_error
+    expect { my_account.deposit_money(20) }.not_to raise_error
   end
 
 end
