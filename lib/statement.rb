16d4511940ff3211c70c8a70c48608feaca5bcff
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-15 15:35:10 +0000

    updating for getting values from the transaction instance object

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,8 +1,11 @@
+require 'date'
+
 class Statement
+
   def print_statement(bank_account)
     puts 'Date || Credit || Debit || Balance'
     bank_account.account_transactions.reverse_each do |transaction|
-      puts "#{transaction[0]} || #{transaction[1]} || #{transaction[2]} || #{transaction[3]}"
+      puts "#{Date.new.format_date(transaction.date)} || #{transaction.credit} || #{transaction.debit} || #{transaction.balance}"
     end
   end
 end
