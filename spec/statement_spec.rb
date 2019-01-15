716ca49c6f17e7ca14315a6c2ad25087b1120968
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-15 15:26:47 +0000

    updating statement spec, freezing time.now, updating test for new transaction class

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,17 +1,23 @@
 # frozen_string_literal: true
 
 require 'statement'
+require 'transaction'
 
 describe Statement do
-  let(:luisa_account) { double :bank_account, name: 'Luisa', balance: 20, account_transactions: \
-                            [['14/01/2019', 'credit', 200, 200],['10/01/2019', 'debit', 100, 100]]
-  }
+  before do
+    @time = Time.now
+    Time.stub(:now) { @time }
+  end
+
+  let(:transaction) { instance_double('Transaction', date: Time.now, credit: 200, debit: 0, balance: 200) }
+  let(:luisa_account) { double :bank_account, name: 'Luisa', balance: 200, account_transactions: [transaction]}
   let(:luisa_statement) { described_class.new }
 
+
   describe '#print_statement' do
-    it 'print statement showing transactions date, type, amount and balance' do
-      expect(luisa_statement.print_statement(luisa_account)).to eq \
-        [['14/01/2019', 'credit', 200, 200], ['10/01/2019', 'debit', 100, 100]]
+    it 'return statement transactions with date, type, amount and balance' do
+      expect(luisa_statement.print_statement(luisa_account)).to eq [transaction]
     end
   end
 end
+
