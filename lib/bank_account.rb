ae2dbd5c7d505140508ad2c862349c00312b974d
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-16 14:58:41 +0000

    refactoring for the new approach adding dependency injection and now transactions log will be available for the bank account

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,31 +1,28 @@
-require_relative 'transaction'
+require_relative 'transactions'
+
 
 class BankAccount
-  attr_reader :account_transactions, :balance
+  attr_reader :transactions, :balance
 
-  def initialize
+  def initialize(transactions = Transactions.new)
     @balance = 0
-    @account_transactions = []
+    @transactions = transactions
   end
 
   def deposit_money(amount, time_now = Time.now)
     correct_input?(amount)
     @balance += amount
-    @account_transactions << create_transaction(time_now, format('%.2f', amount), nil, format('%.2f', @balance))
+    @transactions.store_credit_transaction(time_now, format('%.2f', amount), format('%.2f', @balance))
   end
 
   def withdraw_money(amount, time_now = Time.now)
     correct_input?(amount)
     insufficient_funds?(@balance, amount)
     @balance -= amount
-    @account_transactions << create_transaction(time_now, nil, format('%.2f', amount), format('%.2f', @balance))
+    @transactions.store_debit_transaction(time_now, format('%.2f', amount), format('%.2f', @balance))
   end
 
   private
-
-    def create_transaction(date, credit, debit, balance)
-      Transaction.new(date, credit, debit, balance)
-    end
 
     def insufficient_funds?(balance, amount)
       raise "You don't have sufficient balance to withdraw." if balance < amount
