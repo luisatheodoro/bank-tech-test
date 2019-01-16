cd1ac0ae12dbcc2d69ab262a1acac1b629ec86be
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-16 16:56:52 +0000

    deleting obsolete test and taking off the hard dependency on transactions

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -6,55 +6,46 @@ describe BankAccount do
     @time = Time.now.strftime('%d/%m/%Y')
     allow(Time).to receive(:now).and_return(@time)
   end
-
-  let(:account) { described_class.new() }
+  let(:transaction_1) do
+    double(Transactions, transactions_log: [{ balance: '30.00', credit: format('%.2f', '30.00'), date: @time, debit: nil }])
+    end
+  let(:transaction_2) do
+    double(Transactions, transactions_log: [{ balance: '30.00', credit: format('%.2f', '30.00'), date: @time, debit: nil },
+                                            { balance: '10.00', debit: format('%.2f', '20.00'), date: @time, credit: nil }])
+  end
+  let(:account_1) { described_class.new(transaction_1) }
+  let(:account_2) { described_class.new(transaction_2) }
 
   describe '#deposit_money' do
     it 'cannot deposit negative amount' do
-      expect { account.deposit_money(-20.00) }.to raise_error \
+      expect { account_1.deposit_money(-20.00) }.to raise_error \
         'You can\'t deposit negative number or 0'
     end
 
     it 'will raise an error if typing characters' do
-      expect { account.deposit_money('@£kk') }.to raise_error \
+      expect { account_1.deposit_money('@£kk') }.to raise_error \
         'You can\'t input other value than number'
     end
 
     it 'stores the transaction inside the log' do
-      account.deposit_money(30.00)
-      account_transactions = account.transactions
+      account_transactions = account_1.transactions
       expect(account_transactions.transactions_log).to eq [{ balance: '30.00', credit: '30.00', date: "#{@time}", debit: nil }]
-    end
-  end
-
-  describe '#balance' do
-    it 'should add the amount deposited to the balance' do
-      account.deposit_money(20.00)
-      expect(account.balance).to eq 20.00
-    end
-
-    it 'should subtract the amount withdrawn to the balance' do
-      account.deposit_money(30.00)
-      account.withdraw_money(20.00)
-      expect(account.balance).to eq 10.00
     end
   end
 
   describe '#withdraw money' do
     it 'cannot withdraw negative amount' do
-      expect { account.withdraw_money(-20) }.to raise_error 'You can\'t deposit negative number or 0'
+      expect { account_1.withdraw_money(-20) }.to raise_error 'You can\'t deposit negative number or 0'
     end
 
     it 'will raise an error if typing characters' do
-      expect { account.withdraw_money('@£kk') }.to raise_error 'You can\'t input other value than number'
+      expect { account_1.withdraw_money('@£kk') }.to raise_error 'You can\'t input other value than number'
     end
 
     it 'stores the transaction inside the log' do
-      account.deposit_money(30.00)
-      account.withdraw_money(20)
-      account_transactions = account.transactions
-      expect(account_transactions.transactions_log).to eq [{ balance: "30.00", credit: "30.00", date: "#{@time}", debit: nil },\
-                                                          { balance: "10.00", credit: nil, date: "#{@time}", debit: "20.00" }]
+      account_transactions = account_2.transactions
+      expect(account_transactions.transactions_log).to eq [{ balance: '30.00', credit: '30.00', date: "#{@time}", debit: nil },\
+                                                          { balance: '10.00', credit: nil, date: "#{@time}", debit: '20.00' }]
     end
   end
 end
