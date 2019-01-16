bce5bcd8b2a067516cfd65286a5df88ad9281230
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-16 17:21:52 +0000

    Adding feature tests for deposit and withdraw tests

--- spec/feature_tests/deposit_money_spec.rb
+++ spec/feature_tests/deposit_money_spec.rb
@@ -1,5 +1,13 @@
 feature 'Deposit money' do
+  before do
+    @time = Time.now.strftime('%d/%m/%Y')
+    allow(Time).to receive(:now).and_return(@time)
+  end
   subject(:account) { BankAccount.new }
+
+  scenario 'After user deposit, the transaction will be stores inside transactions' do
+    expect(account.deposit_money(20)).to eq [{ date: "#{@time}", credit: "20.00", debit: nil, balance: "20.00" }]
+  end
   scenario 'User can\'t deposit amount equal zero' do
     expect { account.deposit_money(0) }.to raise_error 'You can\'t deposit negative number or 0'
   end
@@ -7,7 +15,6 @@ feature 'Deposit money' do
   scenario 'User can\'t deposit amount less than zero' do
     expect { account.deposit_money(-1) }.to raise_error 'You can\'t deposit negative number or 0'
   end
-
   scenario 'User can\'t type string' do
     expect { account.deposit_money('20.50') }.to raise_error 'You can\'t input other value than number'
   end
--- spec/feature_tests/withdraw_money_spec.rb
+++ spec/feature_tests/withdraw_money_spec.rb
@@ -1,6 +1,14 @@
 feature 'Withdraw money' do
+  before do
+    @time = Time.now.strftime('%d/%m/%Y')
+    allow(Time).to receive(:now).and_return(@time)
+  end
   subject(:account) { BankAccount.new }
 
+  scenario 'After user withdraw, the transaction can be seen inside transactions' do
+    account.deposit_money(40)
+    expect(account.withdraw_money(20)).to eq [{ date: "#{@time}", credit: "40.00", debit: nil, balance: "40.00"}, {date: "#{@time}", credit: nil, debit: "20.00", balance: "20.00"}]
+  end
   scenario 'User can\'t withdraw amount equal zero' do
     expect { account.withdraw_money(0) }.to raise_error 'You can\'t deposit negative number or 0'
   end
