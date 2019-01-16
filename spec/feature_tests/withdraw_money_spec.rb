f03625ec86c82f01ca0cc62af101649c9146f351
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-16 16:58:26 +0000

    commiting transaction specs

--- spec/feature_tests/withdraw_money_spec.rb
+++ spec/feature_tests/withdraw_money_spec.rb
@@ -1,10 +1,5 @@
 feature 'Withdraw money' do
   subject(:account) { BankAccount.new }
-  scenario 'User can withdraw money and see it subtracted from balance' do
-    account.deposit_money(20.50)
-    account.withdraw_money(1.20)
-    expect(account.balance).to eq 19.30
-  end
 
   scenario 'User can\'t withdraw amount equal zero' do
     expect { account.withdraw_money(0) }.to raise_error 'You can\'t deposit negative number or 0'
