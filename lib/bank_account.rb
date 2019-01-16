44c62564fdadaed24ddb74edf3f309419f889312
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-16 11:00:04 +0000

    deleting functionality that is not part of requirement. adding private methods

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,37 +1,38 @@
-# frozen_string_literal: true.
-
-require 'date'
 require_relative 'transaction'
 
 class BankAccount
-  MAX_WITHDRAWAL = 100
   attr_reader :account_transactions, :balance
 
-  def initialize(client_name)
-    @client_name = client_name
+  def initialize
     @balance = 0
     @account_transactions = []
   end
 
   def deposit_money(amount, time_now = Time.now)
+    correct_input?(amount)
     @balance += amount
-    @account_transactions << Transaction.new(time_now, amount, '', @balance)
-    "You deposited £#{amount} on #{Date.new.format_date(time_now)}. Your account balance is £#{@balance}"
+    @account_transactions << create_transaction(time_now, format('%.2f', amount), nil, format('%.2f', @balance))
   end
 
   def withdraw_money(amount, time_now = Time.now)
+    correct_input?(amount)
     insufficient_funds?(@balance, amount)
-    exceed_max_withdrawal?(amount)
     @balance -= amount
-    @account_transactions << Transaction.new(time_now, '', amount, @balance)
-    "You withdraw £#{amount} on #{Date.new.format_date(time_now)}. Your account balance is £#{@balance}"
+    @account_transactions << create_transaction(time_now, nil, format('%.2f', amount), format('%.2f', @balance))
   end
 
-  def insufficient_funds?(balance, amount)
-    raise "You don't have sufficient balance to withdraw. Your account balance is £#{balance}" if balance < amount
-  end
+  private
 
-  def exceed_max_withdrawal?(amount)
-    raise 'You can only withdraw £100 pounds per transaction' if amount > MAX_WITHDRAWAL
-  end
+    def create_transaction(date, credit, debit, balance)
+      Transaction.new(date, credit, debit, balance)
+    end
+
+    def insufficient_funds?(balance, amount)
+      raise "You don't have sufficient balance to withdraw." if balance < amount
+    end
+
+    def correct_input?(amount)
+      raise "You can't input other value than number" if amount.is_a? String
+      raise "You can't deposit negative number or 0" if amount <= 0
+    end
 end
