7c7651e44928fb0e2d2058dd2cc6f69ef5753d28
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-16 11:19:42 +0000

    improve feature test for printing statement

--- spec/feature_tests/user_can_print_statement_spec.rb
+++ spec/feature_tests/user_can_print_statement_spec.rb
@@ -2,8 +2,10 @@ feature 'Print Statement' do
   it 'will print all account transactions' do
     account = BankAccount.new
     account.deposit_money(20.00)
+    account.withdraw_money(5.50)
     statement = Statement.new
     expect { statement.print_statement(account) }.to output("date || credit || debit || balance\n"\
-        "16/01/2019 || 20.00 ||  || 20.00\n").to_stdout
+      "16/01/2019 ||  || 5.50 || 14.50\n"\
+      "16/01/2019 || 20.00 ||  || 20.00\n").to_stdout
   end
 end
