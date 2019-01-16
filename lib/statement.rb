7b6e91407dcaafa62a3d672bc32f208f4bb9681e
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-16 14:57:26 +0000

    refactoring for the new approach

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,9 +1,10 @@
 class Statement
   def print_statement(bank_account)
     puts 'date || credit || debit || balance'
-    bank_account.account_transactions.reverse_each do |transaction|
-      puts "#{transaction.date.strftime('%d/%m/%Y')} || #{transaction.credit}"\
-           " || #{transaction.debit} || #{format('%.2f', transaction.balance)}"
+    transactions_obj = bank_account.transactions
+    transactions_obj.transactions_log.reverse_each do |transaction|
+      puts "#{transaction[:date].strftime('%d/%m/%Y')} || #{transaction[:credit]}"\
+           " || #{transaction[:debit]} || #{format('%.2f', transaction[:balance])}"
     end
   end
 end
